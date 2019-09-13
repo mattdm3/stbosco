d58a488c4a4e32a90cdc38fd00f00a86cb8f9db4
Matteo <54079805+mattdm3@users.noreply.github.com>
2019-09-13 15:19:52 -0400

    Updated Site pt1

--- js/creative.js
+++ js/creative.js
@@ -1,3 +1,4 @@
+
 (function($) {
   "use strict"; // Start of use strict
 
@@ -30,14 +31,58 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
+      $(".my_logo_white").attr('src','img/full-logo-blk.png');
     } else {
       $("#mainNav").removeClass("navbar-scrolled");
+      $(".my_logo_white").attr('src','img/full-logo-white.png')
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+
+
+// Contact Form??
+
+function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+      document.getElementById('status').innerHTML = "Name cannot be empty";
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.getElementById('status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.getElementById('status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      document.getElementById('status').innerHTML = "Subject cannot be empty";
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+      document.getElementById('status').innerHTML = "Message cannot be empty";
+      return false;
+  }
+  document.getElementById('status').innerHTML = "Sending...";
+  document.getElementById('contact-form').submit();
+  
+  }
+  
+// Testimonials
+
+
+
+
+
 
   // Magnific popup calls
   $('#portfolio').magnificPopup({
@@ -56,3 +101,4 @@
   });
 
 })(jQuery); // End of use strict
+
